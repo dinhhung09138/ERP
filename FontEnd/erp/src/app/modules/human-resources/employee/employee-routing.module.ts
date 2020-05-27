e3095f2db336ccf4cc7e20bd22eaa50ed3cbb85a
dũng vũ chung <vuchungdunghlym@gmail.com>
2020-05-27 23:22:16 +0700

    build-base-employee

--- FontEnd/erp/src/app/modules/human-resources/employee/employee-routing.module.ts
+++ FontEnd/erp/src/app/modules/human-resources/employee/employee-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { EmployeeContactComponent } from './employee-contact/employee-contact.component';
 import { EmployeeRelationshipComponent } from './employee-relationship/employee-relationship.component';
 import { EmployeeInfoComponent } from './employee-info/employee-info.component';
+import { EmployeeContractComponent } from './employee-contract/employee-contract.component';
+import { EmployeeDiciplineComponent } from './employee-dicipline/employee-dicipline.component';
+import { EmployeeEducationComponent } from './employee-education/employee-education.component';
+import { EmployeeIdentificationComponent } from './employee-identification/employee-identification.component';
+import { EmployeeCommendationComponent } from './employee-commendation/employee-commendation.component';
 
 const routes: Routes = [
   {
@@ -18,6 +23,31 @@ const routes: Routes = [
   {
     path: 'info',
     component: EmployeeInfoComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'contract',
+    component: EmployeeContractComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'dicioline',
+    component: EmployeeDiciplineComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'education',
+    component: EmployeeEducationComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'indentification',
+    component: EmployeeIdentificationComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'commendation',
+    component: EmployeeCommendationComponent,
     pathMatch: 'full',
   },
 ];
