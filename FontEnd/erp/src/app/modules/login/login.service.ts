616b456d2eef79c6ec81a3f043450a010217533c
Anthony Hung <hungtran@saigontechnology.com>
2020-06-06 14:54:39 +0700

    Changing the model.

--- FontEnd/erp/src/app/modules/login/login.service.ts
+++ FontEnd/erp/src/app/modules/login/login.service.ts
@@ -4,16 +4,16 @@ import { LoginModel } from './login.model';
 import { Observable, of } from 'rxjs';
 import { map, tap, catchError } from 'rxjs/operators';
 import { ResponseModel } from 'src/app/core/models/response.model';
-import { ApiUrlModel } from 'src/app/core/models/api-url.model';
+import { APIUrlConstants } from 'src/app/core/constants/api-url.constant';
 
 @Injectable()
 export class LoginService {
 
   url = {
-    login: ApiUrlModel.authenticationApi + 'login'
+    login: APIUrlConstants.authenticationApi + 'login'
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   login(model: LoginModel): Observable<ResponseModel> {
     return this.http.post<ResponseModel>(this.url.login, model).pipe(
