65c132cd8c79ce27a1c1dd9a9d84b9921901f1e3
Anthony Hung <hungtran@saigontechnology.com>
2020-06-12 07:20:16 +0700

    Change table name

--- Database/Tables/Education.sql
+++ Database/Tables/Education.sql
@@ -1,10 +1,10 @@
 
 
-IF OBJECT_ID('dbo.AcademicLevel', 'u') IS NOT NULL 
-  DROP TABLE [dbo].[AcademicLevel];
+IF OBJECT_ID('dbo.Education', 'u') IS NOT NULL 
+  DROP TABLE [dbo].[Education];
 
 GO
-CREATE TABLE [dbo].[AcademicLevel](
+CREATE TABLE [dbo].[Education](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](100) NOT NULL,
 	[Precedence] [int] NOT NULL,
@@ -21,9 +21,9 @@ CREATE TABLE [dbo].[AcademicLevel](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[AcademicLevel] ADD  CONSTRAINT [DF_AcademicLevel_IsActive]  DEFAULT ((1)) FOR [IsActive]
+ALTER TABLE [dbo].[Education] ADD  CONSTRAINT [DF_Education_IsActive]  DEFAULT ((1)) FOR [IsActive]
 GO
-ALTER TABLE [dbo].[AcademicLevel] ADD  CONSTRAINT [DF_AcademicLevel_CreateDate]  DEFAULT (getdate()) FOR [CreateDate]
+ALTER TABLE [dbo].[Education] ADD  CONSTRAINT [DF_Education_CreateDate]  DEFAULT (getdate()) FOR [CreateDate]
 GO
-ALTER TABLE [dbo].[AcademicLevel] ADD  CONSTRAINT [DF_AcademicLevel_Deleted]  DEFAULT ((0)) FOR [Deleted]
+ALTER TABLE [dbo].[Education] ADD  CONSTRAINT [DF_Education_Deleted]  DEFAULT ((0)) FOR [Deleted]
 GO
