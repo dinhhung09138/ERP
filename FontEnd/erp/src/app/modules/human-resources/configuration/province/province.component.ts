67cb83c4b1c0e68166cc0d1a5b364510d3ae2edc
dinhhung09138 <dinhhung09138@gmail.com>
2020-07-29 22:16:05 +0700

    Unit test Province screen.

--- FontEnd/erp/src/app/modules/human-resources/configuration/province/province.component.ts
+++ FontEnd/erp/src/app/modules/human-resources/configuration/province/province.component.ts
@@ -48,24 +48,39 @@ export class ProvinceComponent implements OnInit {
   }
 
   onCreateClick() {
+    if (this.isLoading === true) {
+      return;
+    }
     this.form.onCreateClick();
   }
 
   onImportClick() {
+    if (this.isLoading === true) {
+      return;
+    }
     this.form.onCloseClick();
   }
 
   onExportClick() {
+    if (this.isLoading === true) {
+      return;
+    }
     this.form.onCloseClick();
   }
 
   onUpdateClick(id: number) {
+    if (this.isLoading === true) {
+      return;
+    }
     if (id !== null) {
       this.form.onUpdateClick(id);
     }
   }
 
   onDeleteClick(id: number) {
+    if (this.isLoading === true) {
+      return;
+    }
     this.form.onCloseClick();
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       width: '300px',
@@ -79,6 +94,9 @@ export class ProvinceComponent implements OnInit {
   }
 
   onFilterChange() {
+    if (this.isLoading === true) {
+      return;
+    }
     if (this.searchText.length > 0) {
       this.paging.pageIndex = 0;
     }
@@ -86,6 +104,9 @@ export class ProvinceComponent implements OnInit {
   }
 
   onPageChange(page: PageEvent) {
+    if (this.isLoading === true) {
+      return;
+    }
     this.paging.pageSize = page.pageSize;
     this.paging.pageIndex = page.pageIndex;
     if (page.pageSize !== this.currentPageSize) {
@@ -104,11 +125,11 @@ export class ProvinceComponent implements OnInit {
     filter.paging.pageIndex = this.paging.pageIndex;
     filter.paging.pageSize = this.paging.pageSize;
     this.provinceService.getList(filter).subscribe((response: ResponseModel) => {
-      if (response.responseStatus === ResponseStatus.success) {
+      if (response && response.responseStatus === ResponseStatus.success) {
         this.dataSource.data = response.result.items;
         this.paging.length = response.result.totalItems;
-        this.isLoading = false;
       }
+      this.isLoading = false;
     });
   }
 
@@ -119,7 +140,7 @@ export class ProvinceComponent implements OnInit {
 
     this.provinceService.save(model).subscribe((response: ResponseModel) => {
       this.isLoading = false;
-      if (response) {
+      if (response && response.responseStatus === ResponseStatus.success) {
         this.getList();
       }
     });
