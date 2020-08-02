ba26ae401ca3c22e242baccd7179333634e5c937
Anthony Hung <hungtran@saigontechnology.com>
2020-08-02 18:02:14 +0700

    Get application setting.

--- FontEnd/erp/src/app/core/services/app-load.service.ts
+++ FontEnd/erp/src/app/core/services/app-load.service.ts
@@ -1,3 +1,5 @@
+import { ApplicationConstant } from './../constants/app.constant';
+import { ApplicationSettingInterface } from './../interfaces/app-setting.interface';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UrlSettingInterface } from '../interfaces/url-setting.interface';
@@ -21,6 +23,16 @@ export class AppLoadService {
       APIUrlConstants.authenticationApi = response.authenticationApi;
       APIUrlConstants.hrApi = response.hrApi;
       APIUrlConstants.marketingApi = response.marketingApi;
+      return response;
+    });
+  }
+
+  /**
+   * Get application config
+   */
+  getApplicationConfig(): Promise<any> {
+    return this.http.get<ApplicationSettingInterface>(this.configPath + 'application.config.json').toPromise().then(response => {
+      ApplicationConstant.siteTitle = response.siteTitle;
       return response;
     });
   }
