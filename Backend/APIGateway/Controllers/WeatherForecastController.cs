ee8cf31742f24f8806126d970a1611f4b81b33a9
dinhhung09138 <dinhhung09138@gmail.com>
2020-08-14 04:33:15 +0700

    missing using declareation

--- Backend/APIGateway/Controllers/WeatherForecastController.cs
+++ Backend/APIGateway/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Services.Interfaces;
 using Core.Utility.Filters;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
