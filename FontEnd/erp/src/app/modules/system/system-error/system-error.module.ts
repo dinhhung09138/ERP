cc607fb3d6ded8aa0059238e40922a617b22da0c
Anthony Hung <hungtran@saigontechnology.com>
2020-08-14 17:01:03 +0700

    add all modules and components for system module.

---
+++ FontEnd/erp/src/app/modules/system/system-error/system-error.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SystemErrorComponent } from './system-error.component';
+
+
+
+@NgModule({
+  declarations: [SystemErrorComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SystemErrorModule { }
---
+++ FontEnd/erp/src/app/modules/system/system.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { SystemComponent } from './system.component';
+
+
+
+@NgModule({
+  declarations: [
+    SystemComponent
+  ],
+  imports: [
+    SharedModule
+  ]
+})
+export class SystemModule { }
