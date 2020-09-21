cd51219e3ad5b5e6418fc3da6ea8926db26ec859
dinhhung09138 <dinhhung09138@gmail.com>
2020-09-21 22:24:02 +0700

    refs #33 Show Employee Name and Avatar after login

--- FontEnd/erp/src/app/shared/components/header/header.component.ts
+++ FontEnd/erp/src/app/shared/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { SessionContext } from '../../../core/session.context';
+import { UserInfoModel } from '../../../core/models/user-info.model';
 
 @Component({
   selector: 'app-header',
@@ -10,16 +11,26 @@ import { SessionContext } from '../../../core/session.context';
 })
 export class HeaderComponent implements OnInit {
 
+  userInfo: UserInfoModel;
+
   constructor(
     private router: Router,
     private context: SessionContext) { }
 
   ngOnInit(): void {
+    this.userInfo = this.context.getUser();
   }
 
   onLogoutClick() {
     this.context.clearToken();
     this.router.navigate(['/login']);
+  }
+
+  getAvatar() {
+    if (this.userInfo && this.userInfo.avatar) {
+      return this.userInfo.avatar;
+    }
+    return '../../../../assets/images/no-image.png';
   }
 
 }
