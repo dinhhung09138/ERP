6cc6fb804284e7e89ac5a4ddc5aaa11ca11ea7c2
dinhhung09138 <dinhhung09138@gmail.com>
2020-09-21 22:10:08 +0700

    refs #30 Logout function

--- FontEnd/erp/src/app/shared/components/header/header.component.ts
+++ FontEnd/erp/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { SessionContext } from '../../../core/session.context';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private context: SessionContext) { }
 
   ngOnInit(): void {
+  }
+
+  onLogoutClick() {
+    this.context.clearToken();
+    this.router.navigate(['/login']);
   }
 
 }
